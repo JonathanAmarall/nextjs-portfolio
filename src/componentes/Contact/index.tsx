a0e8c2890c37057c787ae791b70fbc14d463b44b
JonathanAmarall <jhouamaral95@gmail.com>
2022-04-05 18:22:13 -0300

    refactor: componentized Contact page

---
+++ src/componentes/Contact/index.tsx
@@ -0,0 +1,53 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Textarea,
+} from '@chakra-ui/react';
+import { Title } from '../Title';
+
+export function Contact() {
+  return (
+    <Flex
+      dir="column"
+      flexDir="column"
+      w={'98vw'}
+      h="700px"
+      justify="center"
+      align={'center'}
+    >
+      <Box id="contact" alignItems={'center'}>
+        <Title name="Contact" />
+      </Box>
+      <Flex
+        as="form"
+        width={'90%'}
+        maxW={600}
+        flexDir="column"
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+      >
+        <FormControl pb={3}>
+          <FormLabel htmlFor="name">Name</FormLabel>
+          <Input id="name" type="text" />
+        </FormControl>
+        <FormControl pb={3}>
+          <FormLabel htmlFor="email">Email address</FormLabel>
+          <Input id="email" size={'md'} type="email" />
+        </FormControl>
+        <FormControl pb={3}>
+          <FormLabel htmlFor="message">Message</FormLabel>
+          <Textarea id="message" size="sm" />
+        </FormControl>
+        <Button type="submit" mt="6" colorScheme="purple">
+          Send Message
+        </Button>
+      </Flex>
+    </Flex>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ import { Title } from '../componentes/Title';
 import CarouselSlide from '../componentes/CarouselSlide';
 import ScrollToTop from 'react-scroll-to-top';
 import { About } from '../componentes/About';
+import { Contact } from '../componentes/Contact';
 
 export default function Home() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -30,46 +31,7 @@ export default function Home() {
       <About />
       {/* Page Skills */}
 
-      {/* Page Contact */}
-      <Flex
-        dir="column"
-        flexDir="column"
-        w={'98vw'}
-        h="700px"
-        justify="center"
-        align={'center'}
-        className={styles.backgroundImageInverse}
-      >
-        <Box id="contact" alignItems={'center'}>
-          <Title name="Contact" />
-        </Box>
-        <Flex
-          as="form"
-          width={'100%'}
-          maxW={600}
-          flexDir="column"
-          bg="gray.800"
-          p="8"
-          borderRadius={8}
-        >
-          <FormControl pb={3}>
-            <FormLabel htmlFor="name">Name</FormLabel>
-            <Input id="name" type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="email">Email address</FormLabel>
-            <Input id="email" type="email" />
-            <FormHelperText>We'll never share your email.</FormHelperText>
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="message">Message</FormLabel>
-            <Textarea id="message" size="sm" />
-          </FormControl>
-          <Button type="submit" mt="6" bg="var(--primary)" colorScheme="purple">
-            Send Message
-          </Button>
-        </Flex>
-      </Flex>
+      <Contact />
       {/* Page Projects */}
       <Flex
         id="projects"
